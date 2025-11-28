9d95b7541df6c2b7fad1bffb921d74c5efb6b127
shahd351 <shahdd.kh@gmail.com>
2025-11-28 11:11:57 +0300

    3
    3

--- theme.js
+++ theme.js
@@ -1,25 +1,41 @@
 const toggle = document.getElementById("theme-toggle");
 const themeLink = document.getElementById("theme-style");
 
-// تحميل الثيم من LocalStorage
-if (localStorage.getItem("theme") === "dark") {
-    themeLink.href = "style-dark.css";
-    if (toggle) toggle.checked = true;
+
+let savedTheme = localStorage.getItem("theme");
+
+switch (savedTheme) {
+    case "dark":
+	
+        themeLink.href = "style-dark.css";
+        if (toggle) toggle.checked = true;
+        break;
+
+    case "light":
+	
+        themeLink.href = "style-light.css";
+        break;
+
+    default:
+	
+        themeLink.href = "style-light.css";
+        break;
 }
 
-// تغيير الثيم بدون أي تعقيد
-if (toggle) {
-    toggle.onchange = function() {
 
-        if (toggle.checked === true) {
+if (toggle) {
+    toggle.onchange = function () {
+
+      
+        if (toggle.checked) {
             themeLink.href = "style-dark.css";
             localStorage.setItem("theme", "dark");
-        } 
+        }
 
+        
         else {
             themeLink.href = "style-light.css";
             localStorage.setItem("theme", "light");
         }
-
     };
 }
